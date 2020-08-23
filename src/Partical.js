272fb269ccd86814e0081bc3ee2e9bccf71b9b11
LW11995 <=>
2020-08-23 11:36:21 +0700

    basic root animation

--- src/Partical.js
+++ src/Partical.js
@@ -38,3 +38,58 @@ export class Partical {
     // ctx.restore();
   }
 }
+
+export class Root {
+  constructor({
+    x,
+    y,
+    rgba,
+    rootX = 0,
+    rootY = 0,
+    distance = 80,
+    ctx,
+    alpha = 1,
+    rotate,
+  }) {
+    this.x = x;
+    this.y = y;
+    this.rootX = rootX;
+    this.rootY = rootY;
+    this.distance = distance;
+    this.rgba = rgba;
+    this.ctx = ctx;
+
+    this.rotate = rotate;
+    this.alpha = alpha;
+    this.speedX = 0;
+    this.speedY = 0;
+  }
+  draw() {
+    this.caculateNextFrame();
+    if (this.radius > 0) {
+      if (this.image) {
+        this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      } else if (this.rgba) {
+        const { r, g, b, a } = this.rgba;
+        this.ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${Math.random()})`;
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        this.ctx.strokeWidth = 1;
+        this.ctx.fill();
+        this.ctx.stroke();
+      }
+    }
+    requestAnimationFrame(this.draw.bind(this));
+  }
+  caculateNextFrame() {
+    this.speedX += (Math.random() - 0.5) / 2;
+    this.speedY += (Math.random() - 0.5) / 2;
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    const distanceX = this.x - this.rootX;
+    const distanceY = this.y - this.rootY;
+    const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+    this.radius = ((-distance / this.distance + 1) * this.distance) / 10;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-import { Partial } from "./Partical";
+import { Partial, Root } from "./Partical";
+import { loadImage, getRandomRgb } from "./helper";
+import { Mouse } from "./Mouse";
+
+////////////////////////////////
+const width = window.innerWidth;
+const height = window.innerHeight;
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+////////////////////////////////
+canvas.width = width;
+canvas.height = height;
+const edge = 80;
+////////////////////////////////
+function onMouseMove({ x, y }) {
+  const rootX = x;
+  const rootY = y;
+  const root = new Root({ x, y, rootX, rootY, rgba: getRandomRgb(), ctx });
+  root.draw();
+}
+
+const mouse = new Mouse().listenMouseMouveEvent(onMouseMove);
